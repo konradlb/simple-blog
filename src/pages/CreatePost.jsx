854f7de67e07f2ff3d571ddd8b25ab59d7ac8345
Konrad Lubowicki <konrad@lubowicki.pl>
2022-01-25 14:45:54 +0100

    Add create page form and basic styles

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,7 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../firebase-config";
+import { useNavigate } from "react-router-dom";
 
 function CreatePost() {
-  return <div>CreatePost</div>;
+  const [title, setTitle] = useState("");
+  const [postImageUrl, setPostImageUrl] = useState("");
+  const [postText, setPostText] = useState("");
+  const [postSlug, setPostSlug] = useState("");
+
+  const postsCollectionRef = collection(db, "posts");
+
+  let navigate = useNavigate();
+
+  const createPost = async () => {
+    await addDoc(postsCollectionRef, {
+      title: title,
+      postImageUrl: postImageUrl,
+      postText: postText,
+      postSlug: postSlug,
+    });
+    navigate("/");
+  };
+
+  return (
+    <div className="createPostPage">
+      <div className="cpContainer">
+        <h1>Create a post</h1>
+        <div className="inputGp">
+          <label>Title:</label>
+          <input
+            placeholder="Title..."
+            onChange={(event) => {
+              setTitle(event.target.value);
+            }}
+          ></input>
+        </div>
+        <div className="inputGp">
+          <label>Post image url:</label>
+          <input
+            placeholder="Url.."
+            onChange={(event) => {
+              setPostImageUrl(event.target.value);
+            }}
+          ></input>
+        </div>
+
+        <div className="inputGp">
+          <label>Slug:</label>
+          <input
+            placeholder="Slug.."
+            onChange={(event) => {
+              setPostSlug(event.target.value);
+            }}
+          ></input>
+        </div>
+
+        <div className="inputGp">
+          <label>Post:</label>
+          <textarea
+            placeholder="Post.."
+            onChange={(event) => {
+              setPostText(event.target.value);
+            }}
+          ></textarea>
+        </div>
+        <button onClick={createPost}> Submit </button>
+      </div>
+    </div>
+  );
 }
 
 export default CreatePost;
