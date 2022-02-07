e57106c9a761c4226015430194f6c573de480b60
Konrad Lubowicki <konrad@lubowicki.pl>
2022-02-07 09:20:22 +0100

    Add simple footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function Footer() {
-  return <div>footer</div>;
+  return (
+    <footer>
+      <p>
+        &copy; {new Date().getFullYear()} <span>Konrad Lubowicki</span>
+      </p>
+    </footer>
+  );
 }
 
 export default Footer;
